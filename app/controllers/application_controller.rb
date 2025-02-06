4fc3ab62fe090ea0d596f2d9dcd836e09e9fb9f9
Rodolfo Araya <rudolfaray2@gmail.com>
2025-02-06 17:50:08 -0300

    Se agregan seeds de artículos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  include Pundit
+  include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+100.times do
+  # default_user
+  default_user = User.find_or_create_by(email: "unknown@example.com") do |user|
+    user.password = SecureRandom.hex(8)
+  end
+  # Genera un título con 6 palabras
+  title = Faker::Lorem.sentence(word_count: 6)
+
+  # Genera 3 párrafos para el cuerpo, cada uno con 5 oraciones
+  paragraphs = Array.new(3) { Faker::Lorem.paragraph(sentence_count: 5) }
+
+  body = "**#{title}**\n\n#{paragraphs.join("\n\n")}\n\n*Fin del artículo.*"
+
+  Article.create!(title: title, body: body, user_id: default_user.id)
+end
